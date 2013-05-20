44b72521581bf1eda932e8edb1b1a6928bd499bd
Andrey Malakhov <a.malakhov@mobidev.biz>
2013-05-20 14:16:36 +0300

    Rename.

--- src/com/mda/datagate/NetCommandListener.java
+++ src/com/mda/datagate/NetCommandListener.java
@@ -2,5 +2,5 @@ package com.mda.datagate;
 
 public interface NetCommandListener {
     public void onComplete(AbstractRequest request, Object response);
-    public boolean onError(Status errorId, Object responseObj);
+    public boolean onError(Status error, Object responseObj);
 }
