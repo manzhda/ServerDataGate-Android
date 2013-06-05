878f68664a5f4eba9ffc17bd034074e110a5a87a
Andrey Malakhov <a.malakhov@mobidev.biz>
2013-06-05 14:20:56 +0300

    Add json and multipart requests.

---
+++ src/com/mda/datagate/JSONRequest.java
@@ -0,0 +1,68 @@
+package com.mda.datagate;
+
+import android.content.Context;
+import com.mda.datagate.utils.MyLog;
+import org.apache.http.HttpEntityEnclosingRequest;
+import org.apache.http.client.methods.HttpEntityEnclosingRequestBase;
+import org.apache.http.client.methods.HttpPost;
+import org.apache.http.client.methods.HttpPut;
+import org.apache.http.client.methods.HttpRequestBase;
+import org.apache.http.entity.StringEntity;
+import org.apache.http.protocol.HTTP;
+import org.json.JSONObject;
+
+import java.io.UnsupportedEncodingException;
+
+public abstract class JSONRequest<T> extends AbstractRequest<T> {
+    private static final String TAG = Controller.class.getSimpleName();
+
+    private static final String CONTENT_TYPE_JSON = "application/json";
+
+    public enum Type {
+        POST,
+        PUT
+    }
+
+    private final Type mType;
+
+    public JSONRequest(Context context, NetCommandListener listener, Type type) {
+        super(context, listener);
+        mType = type;
+    }
+
+    protected abstract JSONObject getJsonBody();
+
+    @Override
+    protected HttpRequestBase createHttpRequest(String url) {
+        HttpEntityEnclosingRequestBase httpRequest = null;
+        switch (mType) {
+            case POST:
+                httpRequest = new HttpPost(url);
+                break;
+
+            case PUT:
+                httpRequest = new HttpPut(url);
+                break;
+        }
+        addJsonEntity(httpRequest);
+        httpRequest.setHeader("Accept", CONTENT_TYPE_JSON);
+        httpRequest.setHeader(HTTP.CONTENT_TYPE, CONTENT_TYPE_JSON);
+        return httpRequest;
+    }
+
+    private void addJsonEntity(HttpEntityEnclosingRequest httpRequest) {
+        JSONObject body = getJsonBody();
+
+        if (body == null) {
+            return;
+        }
+
+        try {
+            StringEntity strBody = new StringEntity(body.toString(), "utf-8");
+            httpRequest.setEntity(strBody);
+            MyLog.vt(TAG, body);
+        } catch (UnsupportedEncodingException e) {
+            e.printStackTrace();
+        }
+    }
+}
---
+++ src/com/mda/datagate/MultipartRequest.java
@@ -0,0 +1,39 @@
+package com.mda.datagate;
+
+import android.content.Context;
+import org.apache.http.client.methods.HttpEntityEnclosingRequestBase;
+import org.apache.http.client.methods.HttpPost;
+import org.apache.http.client.methods.HttpPut;
+import org.apache.http.client.methods.HttpRequestBase;
+import org.apache.http.entity.mime.MultipartEntity;
+
+public abstract class MultipartRequest<T> extends AbstractRequest<T> {
+    private final JSONRequest.Type mType;
+
+    public MultipartRequest(Context context, NetCommandListener listener, JSONRequest.Type type) {
+        super(context, listener);
+        mType = type;
+    }
+
+    @Override
+    protected HttpRequestBase createHttpRequest(String url) {
+        HttpEntityEnclosingRequestBase httpRequest = null;
+        // TODO: code duplication
+        switch (mType) {
+            case POST:
+                httpRequest = new HttpPost(url);
+                break;
+
+            case PUT:
+                httpRequest = new HttpPut(url);
+                break;
+        }
+
+        httpRequest.addHeader("Accept", "application/json");
+        MultipartEntity entity = createMultipartEntity();
+        httpRequest.setEntity(entity);
+        return httpRequest;
+    }
+
+    protected abstract MultipartEntity createMultipartEntity();
+}
